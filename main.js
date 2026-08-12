241ffae7167c8624101f7fbb428b5774e7a4314c
Quan Mac <qmac267@gmail.com>
2026-08-12 13:03:58 +0700

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ loader.load(
         VRMUtils.rotateVRM0(vrm);
 
         vrm.scene.rotation.y = Math.PI;
-        vrm.scene.position.set(0, 1.25, -3);
+        vrm.scene.position.set(0, 0.7, -1.25);
 
         scene.add(vrm.scene);
         currentVrm = vrm;
