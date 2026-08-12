4c6c7cbbeb5448405f61c12dea7d4006bf8d1c92
Quan Mac <qmac267@gmail.com>
2026-08-12 10:49:36 +0700

    Refactor main.js to improve structure and readability

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ loader.load(
         VRMUtils.rotateVRM0(vrm);
 
         vrm.scene.rotation.y = Math.PI;
-        vrm.scene.position.set(0, 0.6, -1);
+        vrm.scene.position.set(0, 2, -1);
 
         scene.add(vrm.scene);
         currentVrm = vrm;
